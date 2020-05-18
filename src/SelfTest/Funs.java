06f663f3754af89e8a9af5b3ec5b4416c801ecb2
toastyhatesthsea <toastyhatesthesea@gmail.com>
2020-05-17 23:51:12 -0700

    Added reverse array function

--- src/SelfTest/Funs.java
+++ src/SelfTest/Funs.java
@@ -7,8 +7,7 @@ public class Funs
         if (n <= 1)
         {
             System.out.println("Hurrah");
-        }
-        else
+        } else
         {
             System.out.println("Hip");
             cheers(n - 1);
@@ -31,10 +30,49 @@ public class Funs
         if (num <= 9)
         {
             return 1;
-        }
-        else
+        } else
         {
             return 1 + digits(num / 10);
+        }
+    }
+
+    /**
+     * Sum of the reciprocals of the first n positive integers
+     *
+     * @param n Non-negative integer
+     * @return
+     */
+    public static double sumover(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        } else if (n == 0)
+        {
+            return 0;
+        } else if (n < 0)
+        {
+            throw new IllegalArgumentException("Number must be positive");
+        } else
+        {
+            return (1 / (double) n) + sumover(n - 1);
+        }
+    }
+
+    public static void reverseArray(char[] anArray, int bound1, int bound2)
+    {
+        if (bound1 < 0 || bound2 >= anArray.length)
+        {
+            throw new IllegalArgumentException("Not valid bounds of array");
+        }
+
+        if ((bound1 < bound2))
+        {
+            char firstChar = anArray[bound1];
+            anArray[bound1] = anArray[bound2];
+            anArray[bound2] = firstChar;
+
+            reverseArray(anArray, bound1 + 1, bound2 - 1);
         }
     }
 
@@ -47,6 +85,12 @@ class FunsTest
         //Funs.cheers(3);
         //Funs.asterisk(3);
 
-        int answer = Funs.digits(1231234);
+        //int answer = Funs.digits(1231234);
+
+        char[] anArray = {'A', 'B', 'C', 'D'};
+
+        //double answer2 = Funs.sumover(-3);
+
+        Funs.reverseArray(anArray, 1, 3);
     }
 }
